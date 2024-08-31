48ba028c8b55e4fa8ad459af03448c49ab36af33
nowgnas <dev.nowgnas@gmail.com>
2024-09-01 01:41:07 +0900

    [chore]: project initial setting

---
+++ src/main/java/com/chitachat/matching/MatchingApplication.java
@@ -0,0 +1,13 @@
+package com.chitachat.matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chitachat/matching/MatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitachat.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
